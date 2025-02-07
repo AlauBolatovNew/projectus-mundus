d3b987859569bf670c93a0d411d0babcb71a4f12
Alau Bolatov <alau.bol@edu.312school.com>
2025-02-06 22:36:53 -0600

    refactoring

--- root/development.tfvars
+++ root/development.tfvars
@@ -1,2 +1,14 @@
 environment = "development"
 eks_version = "1.32"
+aws_auth_users = [
+  {
+    userarn  = "arn:aws:iam::864899873372:user/terraform"
+    username = "terraform"
+    groups   = ["system:masters"]
+  },
+  {
+    userarn  = "arn:aws:iam::864899873372:user/alau"
+    username = "alau"
+    groups   = ["system:masters"]
+  }
+]
--- root/variables.tf
+++ root/variables.tf
@@ -1,7 +1,30 @@
 variable "environment" {
   type = string
+
+  validation {
+    condition     = var.environment == null
+    error_message = "specify envionment mfcr"
+  }
 }
 
 variable "eks_version" {
   type = string
+
+  validation {
+    condition     = var.environment == null
+    error_message = "specify eks_version mfcr"
+  }
+}
+
+variable "aws_auth_users" {
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  validation {
+    condition     = var.environment == null
+    error_message = "specify aws_auth_users mfcr"
+  }
 }
