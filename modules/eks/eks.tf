09b5e524b9fa3b9a7f9498b0d75b59977f7795c2
Alau Bolatov <alau.bol@edu.312school.com>
2025-02-07 13:07:31 -0600

    refactoring

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -7,6 +7,8 @@ resource "aws_eks_cluster" "demo" {
     authentication_mode = "API_AND_CONFIG_MAP"
   }
 
+  enabled_cluster_log_types = ["api", "audit", "authenticator"]
+
   vpc_config {
     subnet_ids = [
       var.private_subnet_1_id,
@@ -21,34 +23,35 @@ resource "aws_eks_cluster" "demo" {
   depends_on = [aws_iam_role_policy_attachment.demo-AmazonEKSClusterPolicy]
 }
 
-data "aws_eks_cluster" "example" {
-  name = aws_eks_cluster.demo.name
-}
-
-resource "aws_launch_template" "eks_nodes" {
-  name_prefix   = "eks-node-template"
-  image_id      = "ami-0cb91c7de36eed2cb" # EKS-optimized AMI
+# Define the Launch Template for Worker Nodes
+resource "aws_launch_template" "eks_launch_template" {
+  name_prefix   = "eks-node-"
   instance_type = "t3.medium"
+  image_id      = "ami-088b41ffb0933423f" # You can use this as a dynamic value if specified in TFVars
 
-  network_interfaces {
-    associate_public_ip_address = true
+  user_data = base64encode(<<EOF
+#!/bin/bash
+/etc/eks/bootstrap.sh ${aws_eks_cluster.demo.name}
+EOF
+  )
+
+  lifecycle {
+    create_before_destroy = true
   }
 
-  user_data = base64encode(templatefile(
-    "${path.module}/linux_user_data.tpl",
-    {
-      cluster_name        = aws_eks_cluster.demo.name
-      cluster_endpoint    = aws_eks_cluster.demo.endpoint
-      cluster_auth_base64 = data.aws_eks_cluster.example.certificate_authority[0].data
-
-      cluster_service_cidr = data.aws_eks_cluster.example.kubernetes_network_config[0].service_ipv4_cidr
-      cluster_ip_family    = data.aws_eks_cluster.example.kubernetes_network_config[0].ip_family
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      "Name" = "eks-worker-node"
     }
-  ))
+  }
 }
 
-resource "aws_autoscaling_group" "eks_nodes" {
-  name = "eks-node-group"
+# Define the Auto Scaling Group for EKS Worker Nodes
+resource "aws_autoscaling_group" "eks_asg" {
+  desired_capacity = 2
+  min_size         = 5
+  max_size         = 1
   vpc_zone_identifier = [
     var.private_subnet_1_id,
     var.private_subnet_2_id,
@@ -57,43 +60,29 @@ resource "aws_autoscaling_group" "eks_nodes" {
     var.public_subnet_2_id,
     var.public_subnet_3_id
   ]
-  min_size         = 1
-  max_size         = 5
-  desired_capacity = 3
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
 
+  # Define Mixed Instances Policy with On-Demand and Spot Instances
   mixed_instances_policy {
     instances_distribution {
-      on_demand_percentage_above_base_capacity = 20 # 20% On-Demand, 80% Spot
-      spot_allocation_strategy                 = "lowest-price"
-      on_demand_base_capacity                  = 0
+      on_demand_percentage_above_base_capacity = 20
+      spot_allocation_strategy                 = "capacity-optimized"
     }
 
     launch_template {
       launch_template_specification {
-        launch_template_id = aws_launch_template.eks_nodes.id
+        launch_template_id = aws_launch_template.eks_launch_template.id
+        version            = "$Latest"
       }
-
-      override { instance_type = "t3.medium" }
-      override { instance_type = "t3.large" }
-      override { instance_type = "m5.large" }
     }
   }
 
-  tag {
-    key                 = "kubernetes.io/cluster/${var.environment}-eks"
-    value               = "owned"
-    propagate_at_launch = true
-  }
 
+  # Define Tags for Auto Scaling Group (Note the use of "tag" block)
   tag {
-    key                 = "k8s.io/cluster-autoscaler/enabled"
-    value               = "true"
-    propagate_at_launch = true
-  }
-
-  tag {
-    key                 = "k8s.io/cluster-autoscaler/${var.environment}-eks"
-    value               = "owned"
+    key                 = "centos-eks"
+    value               = "eks-asg"
     propagate_at_launch = true
   }
 }
