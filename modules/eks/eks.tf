ebefeafba0b9cda5ec547d9f9e2bbe1c87c4203b
Alau Bolatov <alau.bol@edu.312school.com>
2025-02-07 00:30:19 -0600

    refactoring

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -16,30 +16,13 @@ resource "aws_eks_cluster" "demo" {
       var.public_subnet_2_id,
       var.public_subnet_3_id
     ]
-    security_group_ids = [aws_security_group.eks_nodes.id]
   }
 
   depends_on = [aws_iam_role_policy_attachment.demo-AmazonEKSClusterPolicy]
 }
 
-resource "aws_security_group" "eks_nodes" {
-  name_prefix = "eks-cluster-sg"
-  description = "EKS Cluster Security Group"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+data "aws_eks_cluster" "example" {
+  name = aws_eks_cluster.demo.name
 }
 
 resource "aws_launch_template" "eks_nodes" {
@@ -48,20 +31,22 @@ resource "aws_launch_template" "eks_nodes" {
   instance_type = "t3.medium"
 
   network_interfaces {
-    associate_public_ip_address = true
+    # associate_public_ip_address = true
+    associate_public_ip_address = aws_eks_cluster.demo.
     security_groups             = [aws_security_group.eks_nodes.id]
   }
 
-  user_data = base64encode(
-    <<-EOF
-#!/bin/bash
-set -e
-B64_CLUSTER_CA=${aws_eks_cluster.demo.certificate_authority[0].data}
-API_SERVER_URL=${aws_eks_cluster.demo.endpoint}
-/etc/eks/bootstrap.sh ${aws_eks_cluster.demo.name} --b64-cluster-ca $B64_CLUSTER_CA --apiserver-endpoint $API_SERVER_URL \
-  --ip-family ${aws_eks_cluster.demo.ip_family} --service-${aws_eks_cluster.demo.ip_family}-cidr ${aws_eks_cluster.demo.service_ipv6_cidr}
-    EOF
-  )
+  user_data = base64encode(templatefile(
+    path("./linux_user_data.tpl"),
+    {
+      cluster_name        = aws_eks_cluster.demo.name
+      cluster_endpoint    = aws_eks_cluster.demo.endpoint
+      cluster_auth_base64 = data.aws_eks_cluster.example.certificate_authority[0].data
+
+      cluster_service_cidr = data.aws_eks_cluster.example.kubernetes_network_config[0].service_ipv4_cidr
+      cluster_ip_family    = data.aws_eks_cluster.example.kubernetes_network_config[0].ip_family
+    }
+  ))
 }
 
 resource "aws_autoscaling_group" "eks_nodes" {
@@ -113,40 +98,4 @@ resource "aws_autoscaling_group" "eks_nodes" {
     value               = "owned"
     propagate_at_launch = true
   }
-}
-
-
-resource "aws_eks_node_group" "private-nodes" {
-  cluster_name    = aws_eks_cluster.demo.name
-  node_group_name = "private-nodes"
-  node_role_arn   = aws_iam_role.nodes.arn
-
-  subnet_ids = [
-    var.private_subnet_1_id,
-    var.private_subnet_2_id,
-    var.private_subnet_3_id
-  ]
-
-  capacity_type  = "SPOT"
-  instance_types = ["t3.medium"]
-
-  scaling_config {
-    desired_size = 3
-    max_size     = 5
-    min_size     = 1
-  }
-
-  update_config {
-    max_unavailable = 1
-  }
-
-  labels = {
-    node = "${var.environment}-kubenode"
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
-  ]
 }
--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,3 +1,57 @@
+variable "enable_bootstrap_user_data" {
+  description = "Determines whether the bootstrap configurations are populated within the user data template"
+  type        = bool
+  default     = false
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = ""
+}
+
+variable "cluster_endpoint" {
+  description = "Endpoint of associated EKS cluster"
+  type        = string
+  default     = ""
+}
+
+variable "cluster_auth_base64" {
+  description = "Base64 encoded CA of associated EKS cluster"
+  type        = string
+  default     = ""
+}
+
+variable "cluster_service_cidr" {
+  description = "The CIDR block (IPv4 or IPv6) used by the cluster to assign Kubernetes service IP addresses. This is derived from the cluster itself"
+  type        = string
+  default     = ""
+}
+
+variable "cluster_ip_family" {
+  description = "The IP family used to assign Kubernetes pod and service addresses. Valid values are `ipv4` (default) and `ipv6`"
+  type        = string
+  default     = "ipv4"
+}
+
+variable "additional_cluster_dns_ips" {
+  description = "Additional DNS IP addresses to use for the cluster. Only used when `ami_type` = `BOTTLEROCKET_*`"
+  type        = list(string)
+  default     = []
+}
+
+variable "bootstrap_extra_args" {
+  description = "Additional arguments passed to the bootstrap script. When `ami_type` = `BOTTLEROCKET_*`; these are additional [settings](https://github.com/bottlerocket-os/bottlerocket#settings) that are provided to the Bottlerocket user data"
+  type        = string
+  default     = ""
+}
+
+variable "user_data_template_path" {
+  description = "Path to a local, custom user data template file to use when rendering user data"
+  type        = string
+  default     = ""
+}
+
 variable "vpc_id" {
   type = string
 }
