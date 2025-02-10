7e89f246384b68c49cd9f41dfe13f30aa11e0756
Alau Bolatov <alau.bol@edu.312school.com>
2025-02-10 16:35:00 -0600

    refactoring

--- root/startup.tf
+++ root/startup.tf
@@ -1,20 +1,20 @@
-module "sample_vpc" {
-  source      = "../modules/vpc"
-  cidr        = "10.10.0.0/16"
-  environment = var.environment
-}
+# module "sample_vpc" {
+#   source      = "../modules/vpc"
+#   cidr        = "10.10.0.0/16"
+#   environment = var.environment
+# }
 
-module "kubernetes_cluster" {
-  source = "../modules/eks"
+# module "kubernetes_cluster" {
+#   source = "../modules/eks"
 
-  environment = var.environment
+#   environment = var.environment
 
-  private_subnet_1_id = module.sample_vpc.private_subnet_1_id
-  private_subnet_2_id = module.sample_vpc.private_subnet_2_id
-  private_subnet_3_id = module.sample_vpc.private_subnet_3_id
-  public_subnet_1_id  = module.sample_vpc.public_subnet_1_id
-  public_subnet_2_id  = module.sample_vpc.public_subnet_2_id
-  public_subnet_3_id  = module.sample_vpc.public_subnet_3_id
+#   private_subnet_1_id = module.sample_vpc.private_subnet_1_id
+#   private_subnet_2_id = module.sample_vpc.private_subnet_2_id
+#   private_subnet_3_id = module.sample_vpc.private_subnet_3_id
+#   public_subnet_1_id  = module.sample_vpc.public_subnet_1_id
+#   public_subnet_2_id  = module.sample_vpc.public_subnet_2_id
+#   public_subnet_3_id  = module.sample_vpc.public_subnet_3_id
 
-  depends_on = [module.sample_vpc]
-}
+#   depends_on = [module.sample_vpc]
+# }
