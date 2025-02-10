8b57a84143ed7d078cff8113d3bbc8330e9d96b3
Alau Bolatov <alau.bol@edu.312school.com>
2025-02-10 15:17:50 -0600

    ss

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_cluster" "demo" {
-  name     = "${var.environment}-eks"
+  name     = "centos-eks"
   role_arn = aws_iam_role.demo.arn
 
   access_config {
@@ -20,39 +20,68 @@ resource "aws_eks_cluster" "demo" {
   depends_on = [aws_iam_role_policy_attachment.demo-AmazonEKSClusterPolicy]
 }
 
-resource "aws_eks_node_group" "private-nodes" {
-  cluster_name    = aws_eks_cluster.demo.name
-  node_group_name = "private-nodes"
-  node_role_arn   = aws_iam_role.nodes.arn
+# Define the Launch Template for Worker Nodes
+resource "aws_launch_template" "eks_launch_template" {
+  name_prefix   = "eks-node-"
+  instance_type = "t3.medium"
+  image_id      = "ami-07ef3a922fc0bdb35" # You can use this as a dynamic value if specified in TFVars
 
-  subnet_ids = [
-    var.private_subnet_1_id,
-    var.private_subnet_2_id,
-    var.private_subnet_3_id
-  ]
+  user_data = base64encode(<<EOF
+#!/bin/bash
+/etc/eks/bootstrap.sh ${aws_eks_cluster.demo.name}
+EOF
+  )
 
-  capacity_type  = "SPOT"
-  instance_types = ["t3.medium"]
-
-  scaling_config {
-    desired_size = 3
-    max_size     = 5
-    min_size     = 1
+  lifecycle {
+    create_before_destroy = true
   }
 
-  update_config {
-    max_unavailable = 1
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      "Name" = "eks-worker-node"
+    }
+  }
+}
+
+# Define the Auto Scaling Group for EKS Worker Nodes
+resource "aws_autoscaling_group" "eks_asg" {
+  desired_capacity          = 2
+  min_size                  = 1
+  max_size                  = 5
+  vpc_zone_identifier       = [
+      var.private_subnet_1_id,
+      var.private_subnet_2_id,
+      var.private_subnet_3_id,
+      var.public_subnet_1_id,
+      var.public_subnet_2_id,
+      var.public_subnet_3_id
+    ]
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+
+  # Define Mixed Instances Policy with On-Demand and Spot Instances
+  mixed_instances_policy {
+    instances_distribution {
+      on_demand_percentage_above_base_capacity = 20
+      spot_allocation_strategy                 = "capacity-optimized"
+    }
+
+    launch_template {
+      launch_template_specification {
+        launch_template_id = aws_launch_template.eks_launch_template.id
+        version            = "$Latest"
+      }
+    }
   }
 
-  labels = {
-    node = "${var.environment}-kubenode"
-  }
 
-  depends_on = [
-    aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
-  ]
+  # Define Tags for Auto Scaling Group (Note the use of "tag" block)
+  tag {
+    key                 = "centos-eks"
+    value               = "eks-asg"
+    propagate_at_launch = true
+  }
 }
 
 resource "aws_eks_access_entry" "example" {
