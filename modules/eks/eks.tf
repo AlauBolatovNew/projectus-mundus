3448ef7e1bb3e510a8e676f821ab7c88e9e90b17
Alau Bolatov <alau.bol@edu.312school.com>
2025-02-10 13:40:04 -0600

    refactoring

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -54,3 +54,29 @@ resource "aws_eks_node_group" "private-nodes" {
     aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
+
+resource "aws_eks_access_entry" "example" {
+  cluster_name      = aws_eks_cluster.demo.name
+  principal_arn     = "arn:aws:iam::864899873372:root"
+  type              = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "ss" {
+  cluster_name  = aws_eks_cluster.demo.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = "arn:aws:iam::864899873372:root"
+
+  access_scope {
+    type       = "cluster"
+  }
+}
+
+resource "aws_eks_access_policy_association" "ss" {
+  cluster_name  = aws_eks_cluster.demo.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
+  principal_arn = "arn:aws:iam::864899873372:root"
+
+  access_scope {
+    type       = "cluster"
+  }
+}
