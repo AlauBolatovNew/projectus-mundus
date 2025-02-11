22f16bdc6ca8dc9f5684282be63e81f59418f1d5
Alau Bolatov <alau.bol@edu.312school.com>
2025-02-10 18:01:48 -0600

    kuku

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -44,6 +44,37 @@ resource "aws_eks_cluster" "eks_centos" {
   }
 }
 
+resource "aws_iam_role" "nodes" {
+  name = "eks-node-group-nodes"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes.name
+}
+
+
 # Define the Launch Template for Worker Nodes
 resource "aws_launch_template" "eks_launch_template" {
   name_prefix   = "eks-node-"
@@ -91,6 +122,7 @@ resource "aws_autoscaling_group" "eks_asg" {
       }
     }
   }
+  service_linked_role_arn = aws_iam_role.nodes.arn
 
   # Define Tags for Auto Scaling Group (Note the use of "tag" block)
   tag {
